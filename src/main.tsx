9c1ee40d35f22eeca70906fb11d97abef172a811
AlexeyMachehin <99137228+AlexeyMachehin@users.noreply.github.com>
2023-07-12 19:05:10 +0500

    auth layout

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
